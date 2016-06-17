8fe1aa21a5b0a6d46afc5d1aff8bf464ad7cad50
Mark Biek <mark@viastudio.com>
2016-06-17 11:25:17 -0400

    Coding standards

--- webroot/src/components/blog/index.js
+++ webroot/src/components/blog/index.js
@@ -7,14 +7,14 @@ import request from 'superagent';
 import Loop from '../elements/loop';
 
 var Blog = React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
         return { component: <div /> };
     },
     componentWillMount: function () {
         var self = this;
         request
-            .get('http://api.pura.joel.boom/wp-json/wp/v2/posts')
-            .end(function(err, res) {
+            .get('http://api.pura.mark.boom/wp-json/wp/v2/posts')
+            .end(function (err, res) {
                 var data = JSON.parse(res.text);
 
                 self.setState({ component: <Loop data={ data } bodyClass="blog" /> });
